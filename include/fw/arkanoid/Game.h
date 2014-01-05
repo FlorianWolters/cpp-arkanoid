41970823cc91bfeb43dc0f2cd29bc6909b803647
Florian Wolters <wolters.fl@gmail.com>
2014-01-05 16:24:02 +0100

    Moved class `Game` to dedicated source code file.

---
+++ include/fw/arkanoid/Game.h
@@ -0,0 +1,96 @@
+/**
+ * COPYRIGHT (C) Florian Wolters 2014
+ *
+ * @file
+ * @author Florian Wolters <wolters.fl@gmail.com>
+ */
+
+#ifndef FW_ARKANOID_GAME_H_
+#define FW_ARKANOID_GAME_H_
+
+#include <string>
+#include "SFML/Graphics.hpp"
+
+namespace fw {
+namespace arkanoid {
+
+/**
+ * The class Game encapsulates the application logic for the game "Arkanoid".
+ *
+ * @author Florian Wolters <wolters.fl@gmail.com>
+ */
+class Game {
+ public:
+  /**
+   * Initializes a new instance of the Game class.
+   */
+  Game() {
+    this->window_.setFramerateLimit(kFramerateLimit);
+  }
+
+  /**
+   * Finalizes an instance of the Game class.
+   */
+  ~Game() {
+    this->window_.close();
+  }
+
+  /**
+   * Runs the *Game Loop* of this Game.
+   */
+  void run() {
+    this->is_running_ = true;
+
+    while(this->is_running_) {
+      this->window_.clear(sf::Color::Black);
+      this->handleInput();
+      this->window_.display();
+    }
+  }
+
+  /**
+   * Handles the input for this Game.
+   */
+  void handleInput() {
+    sf::Event event;
+
+    while (this->window_.pollEvent(event)) {
+      switch (event.type) {
+        case sf::Event::Closed:
+          this->is_running_ = false;
+          break;
+        case sf::Event::KeyPressed:
+          if (sf::Keyboard::Escape == event.key.code) {
+            this->is_running_ = false;
+          }
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+ private:
+  /** The width of the window of this Game in pixels. */
+  static constexpr int kWindowWidth{800};
+
+  /** The height of the window of this Game in pixels. */
+  static constexpr int kWindowHeight{600};
+
+  /** The maximum framerate for this Game. */
+  static constexpr int kFramerateLimit{60};
+
+  /** The name of this Game. */
+  const std::string kApplicationName{"Arkanoid"};
+
+  /** Whether this Game is running. */
+  bool is_running_{false};
+
+  /** The window of this Game. */
+  sf::RenderWindow window_{{kWindowWidth, kWindowHeight}, kApplicationName};
+};
+
+} // namespace arkanoid
+} // namespace fw
+
+#endif  // FW_ARKANOID_H_
--- src/main.cc
+++ src/main.cc
@@ -5,61 +5,7 @@
  * @author Florian Wolters <wolters.fl@gmail.com>
  */
 
-#include <string>
-
-#include "SFML/Graphics.hpp"
-
-class Game {
- public:
-  Game() {
-    this->window_.setFramerateLimit(kFramerateLimit);
-  }
-
-  ~Game() {
-    this->window_.close();
-   }
-
-  /**
-   * Runs the loop (called *Game Loop*) of this Game
-   */
-  void run() {
-    this->is_running_ = true;
-
-    while(this->is_running_) {
-      this->window_.clear(sf::Color::Black);
-      this->handleInput();
-      this->window_.display();
-    }
-  }
-
-  void handleInput() {
-    sf::Event event;
-
-    while (this->window_.pollEvent(event)) {
-      switch (event.type) {
-        case sf::Event::Closed:
-          this->is_running_ = false;
-          break;
-        case sf::Event::KeyPressed:
-          if (sf::Keyboard::Escape == event.key.code) {
-            this->is_running_ = false;
-          }
-          break;
-        default:
-          break;
-      }
-    }
-  }
-
-  private:
-   static constexpr int kWindowWidth{800};
-   static constexpr int kWindowHeight{600};
-   static constexpr int kFramerateLimit{60};
-   const std::string kApplicationName{"Arkanoid"};
- 
-   bool is_running_{false};
-   sf::RenderWindow window_{{kWindowWidth, kWindowHeight}, kApplicationName};
-};
+#include "fw/arkanoid/Game.h"
 
 /**
  * Runs the game *Arkanoid*.
@@ -67,7 +13,7 @@ class Game {
  * @return Always `0`.
  */
 int main() {
-  Game{}.run();
+  fw::arkanoid::Game{}.run();
 
   return 0;
 }
