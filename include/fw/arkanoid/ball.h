d032dd9961fc785cac4b0285b5e18a7943bfab71
Florian Wolters <wolters.fl@gmail.com>
2014-01-05 17:33:02 +0100

    Added class skeleton for class `Ball`.

---
+++ include/fw/arkanoid/ball.h
@@ -0,0 +1,22 @@
+/**
+ * COPYRIGHT (C) Florian Wolters 2014
+ *
+ * @file
+ * @author Florian Wolters <wolters.fl@gmail.com>
+ */
+
+#ifndef FW_ARKANOID_BALL_H_
+#define FW_ARKANOID_BALL_H_
+
+namespace fw {
+namespace arkanoid {
+
+class Ball {
+ public:
+ private:
+};
+
+} // namespace arkanoid
+} // namespace fw
+
+#endif  // FW_ARKANOID_BALL_H_
