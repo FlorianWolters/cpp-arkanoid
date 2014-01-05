446dc1bf3f4fd812379129727d7026242e4d9033
Florian Wolters <wolters.fl@gmail.com>
2014-01-05 18:33:24 +0100

    Added comments.

---
+++ include/fw/arkanoid/insets.h
@@ -0,0 +1,15 @@
+#ifndef FW_ARKANOID_POINT_H_
+#define	FW_ARKANOID_POINT_H_
+
+namespace fw {
+namespace arkanoid {
+
+struct Point {
+  float x;
+  float y;
+};
+
+} // namespace arkanoid
+} // namespace fw
+
+#endif  // FW_ARKANOID_POINT_H_
--- include/fw/arkanoid/point.h
+++ include/fw/arkanoid/point.h
@@ -1,11 +1,27 @@
+/**
+ * COPYRIGHT (C) Florian Wolters 2014
+ *
+ * @file
+ * @author Florian Wolters <wolters.fl@gmail.com>
+ */
+
 #ifndef FW_ARKANOID_POINT_H_
 #define	FW_ARKANOID_POINT_H_
 
 namespace fw {
 namespace arkanoid {
 
+/**
+ * A Point representing a location in `(x,y)` coordinate space, specified in
+ * `float` precision.
+ *
+ * @author Florian Wolters <wolters.fl@gmail.com>
+ */
 struct Point {
+  /** The X coordinate of this Point. */
   float x;
+
+  /** The Y coordinate of this Point. */
   float y;
 };
 
